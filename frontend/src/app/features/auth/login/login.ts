6277efff14ab99fa47d430a6b17224bb97f533f5
YousufKhan99 <Yousuf.Khan@danatonline.com>
2026-07-14 18:24:03 +0500

    login files

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { LoginRequest } from '../../../shared/models/auth.model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class Login {}
+export class Login {
+  request: LoginRequest = { email: '', password: '' };
+  isLoading = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.request).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        if (res.isSuccess) {
+          this.router.navigate(['/feed']);
+        } else {
+          this.errorMessage = res.message;
+        }
+      },
+      error: () => {
+        this.isLoading = false;
+        this.errorMessage = 'Something went wrong. Please try again.';
+      }
+    });
+  }
+}
--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrl: './navbar.scss'
 })
-export class Navbar {}
+export class Navbar {
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+  onSearch(event: Event): void {
+  const query = (event.target as HTMLInputElement).value;
+  if (query.trim()) {
+    this.router.navigate(['/feed'], { queryParams: { search: query } });
+  }
+}
+}
